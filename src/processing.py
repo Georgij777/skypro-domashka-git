b5068d7dd66898e5a6e6c6f7cae988dedab380a1
Georgij--777 <georgij--777@yandex.ru>
2025-04-27 23:00:13 +0300

    poetry, annotations,modified README.md

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,15 @@
-def filter_by_state(, state:'EXECUTED'):
+from typing import Iterable
+
+
+def filter_by_state(users_data: Iterable[dict], state: str) -> list:
     """Эта функция возвращает данные по запросу: state == EXECUTED"""
     state_ex = []
     for data in users_data:
-        if data.get('state') == 'EXECUTED':
+        if data.get("state", 0) == "EXECUTED":
             state_ex.append(data)
     return state_ex
 
 
-def sort_by_date(,descending):
+def sort_by_date(users_data: Iterable[dict], descending=True) -> list:
     """Сортирует по дате"""
-    return sorted(users_data, key=lambda x: x.get('date', 0), reverse=descending)
+    return sorted(users_data, key=lambda x: x.get("date", 0), reverse=descending)
